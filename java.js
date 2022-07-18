6a71b4f952862efe64e7c7e20a9332297e3a331d
electrix1337 <95308274+electrix1337@users.noreply.github.com>
2022-07-17 21:55:53 -0400

    1

--- java.js
+++ java.js
@@ -1,4 +1,13 @@
+let joueur = {
+    nom: "",
+    hp: "",
+    meteo: {
+        temperature: 0,
+        luminosite: 0,
+        vents: 0
+    }
+}
+
 $(document).ready( function(event) {
     console.log("test");
 });
-console.log("test");
